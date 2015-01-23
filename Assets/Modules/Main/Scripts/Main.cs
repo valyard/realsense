2e7d0a3c2e561465f8fbe781019ee80870969771
Michael Holub <michael.holub@gmail.com>
2015-01-23 13:27:18 +0300

    sdk, tracker

---
+++ Assets/Modules/Main/Scripts/Main.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour
+{
+    #region Variables
+    public PlayerTracker PlayerTracker;
+    #endregion
+
+    #region Main Methods
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    #endregion
+
+    #region Private Methods
+    #endregion
+}
---
+++ Assets/Modules/Tracker/Scripts/PlayerTracker.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTracker : MonoBehaviour
+{
+    public delegate void PlayerTrackerAction(PlayerTracker pt);
+    public event PlayerTrackerAction OnTrackingStarted;
+    public event PlayerTrackerAction OnTrackingLost;
+    public event PlayerTrackerAction OnBlink;
+
+
+    bool isTrackingFace = false;
+
+    #region Variables
+    #endregion
+
+    #region Main Methods
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+    #endregion
+
+    #region Realsense Callbacks
+    void OnFaceDetected()
+    {
+        startTracking();
+    }
+
+    void OnFaceLost()
+    {
+       stopTracking();
+    }
+
+    void OnEyeClosed()
+    {
+        Debug.Log("Blink");
+        if (isTrackingFace)
+        {
+            blink();
+        }
+    }
+    #endregion
+
+    #region Private Methods
+    void startTracking()
+    {
+        isTrackingFace = true;
+        Debug.Log("Face detected");
+        if (OnTrackingStarted != null)
+        {
+            OnTrackingStarted(this);
+        }
+    }
+
+    void stopTracking()
+    {
+        isTrackingFace = false;
+        Debug.Log("Face lost");
+        if (OnTrackingLost != null)
+        {
+            OnTrackingLost(this);
+        }
+    }
+
+    void blink()
+    {
+        if (OnBlink != null)
+        {
+            OnBlink(this);
+        }
+    }
+    #endregion
+}
