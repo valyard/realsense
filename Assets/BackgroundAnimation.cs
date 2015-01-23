678c98dd2ef2efaf9945d0995f9fa212b2b460fe
Michael Holub <michael.holub@gmail.com>
2015-01-23 16:34:13 +0300

    textures

---
+++ Assets/BackgroundAnimation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundAnimation : MonoBehaviour {
+    public float Speed = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position - Time.deltaTime * Speed * Vector3.right;
+	}
+}
